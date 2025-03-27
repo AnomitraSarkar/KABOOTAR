408583ce1325f2eede1b3f16a744141dc18b5bf3
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-03-27 15:25:41 +0530

    `Added import for register_page.dart and changed home widget to RegisterPage in MyApp`

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kabootar/pages/login_page.dart';
+import 'package:kabootar/pages/register_page.dart';
 import 'package:kabootar/themes/light_mode.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: RegisterPage(),
       theme: lightMode,
     );
   }
