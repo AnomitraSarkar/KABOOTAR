aeb325e888bdb5b379fd138b9723d2e091587c8f
AnomitraSarkar <anomitrasarkar05@gmail.com>
2025-03-29 02:41:09 +0530

    `Removed logout button and function from home page`

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,17 +5,12 @@ import 'package:kabootar/components/my_drawer.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void logout() {
-    final _auth = AuthService();
-    _auth.signOut();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
-        actions: [IconButton(onPressed: logout, icon: Icon(Icons.logout))],
+        // actions: [IconButton(onPressed: logout, icon: Icon(Icons.logout))],
         
       ),
       drawer: MyDrawer(),
